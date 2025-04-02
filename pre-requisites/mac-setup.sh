3b2c012d0126b4af507fc645c9bf7689e1a5f5c8
Joana Carneiro <joana.carneiro@sap.com>
2025-04-02 21:23:39 +0100

    fix: rules to commands

--- pre-requisites/mac-setup.sh
+++ pre-requisites/mac-setup.sh
@@ -5,16 +5,55 @@
 #####################################################################
 
 ## Install package manager
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which -s brew
+if [[ $? != 0 ]] ; then
+    printf -- 'Installing brew.... \n'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    printf -- 'Brew is already installed.\n'
+fi
 
 ## Install source control cli
-brew install git
+which -s git
+if [[ $? != 0 ]] ; then
+    printf -- 'Installing git.... \n'
+    brew install git
+else
+    printf -- 'Git is already installed.\n'
+fi
+
+## Install containerization tool
+which -s git
+if [[ $? != 0 ]] ; then
+    printf -- 'Installing docker.... \n'
+    brew install --cask docker
+else
+    printf -- 'Docker is already installed.\n'
+fi
 
 ## Install kubernetes cli
-brew install kubectl
+which -s git
+if [[ $? != 0 ]] ; then
+    printf -- 'Installing kubectl.... \n'
+    brew install kubectl
+else
+    printf -- 'Kubectl is already installed.\n'
+fi
 
 ## Install Kubernetes in Docker (KinD)
-brew install kind
+which -s kind
+if [[ $? != 0 ]] ; then
+    printf -- 'Installing kind.... \n'
+    brew install kind
+else
+    printf -- 'Kind is already installed.\n'
+fi
 
 ## Install Hotdeploy tool
-brew install skaffold
+which -s skaffold
+if [[ $? != 0 ]] ; then
+    printf -- 'Installing skaffold.... \n'
+    brew install skaffold
+else
+    printf -- 'Skaffold is already installed.\n'
+fi
