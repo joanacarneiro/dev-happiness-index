897b3928ac23e255f73e43c9611d1c4b2def58e5
Joana Carneiro <joana.carneiro@sap.com>
2025-04-03 20:45:43 +0100

    fix: adding first task

--- app/app.js
+++ app/app.js
@@ -8,6 +8,19 @@ app.get('/', (req, res) => {
   res.send('Hello Geek Girls!');
 });
 
+//TODO: fix method to post
+app.put('/data', (req, res) => {
+  const { name, age } = req.body;
+
+  if (!name || !age) {
+    //TODO: add expected return payload  
+    return res.status(400).json();
+  }
+
+  //TODO: add expected return payload
+  res.json();
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
